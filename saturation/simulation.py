8411b80ad4796fc0c312c3ccde015536e0bb77fd
seanmason <mason.s@gmail.com>
2022-07-06 20:42:29 -0500

    Added code and tests for crater generation.

--- saturation/simulation.py
+++ saturation/simulation.py
@@ -1,18 +1,20 @@
 from dataclasses import dataclass
-from typing import Tuple, List
+from typing import Tuple, Iterable, Iterator, Callable
 
 import numpy as np
 
-
 # Type definitions
+from saturation.distributions import ProbabilityDistribution
+
 Location = Tuple[float, float]
+LocationFunc = Callable[[], Location]
 
 # Represents an arc of a crater rim, represented as a tuple of radians
 RimSegment = Tuple[float, float]
-RimSegments = List[RimSegment]
+RimSegments = Iterable[RimSegment]
 
 
-@dataclass
+@dataclass(frozen=True, kw_only=True)
 class Crater:
     """
     Attributes representing a 2D crater for use in simulations.
@@ -24,5 +26,23 @@ class Crater:
     rim_segments: RimSegments
 
 
-def get_crater_locations(n_craters: int) -> np.array:
-    return np.zeros(n_craters)
+def get_crater_location() -> Location:
+    """
+    Returns a uniform random crater location with (x, y) in [0, 1]
+    """
+    r = np.random.rand(2)
+    return r[0], r[1]
+
+
+def get_craters(size_distribution: ProbabilityDistribution,
+                location_func: LocationFunc = get_crater_location) -> Iterator[Crater]:
+    """
+    Returns an infinite iterator of craters. Uses the supplied distribution to generate crater radii.
+    """
+    crater_id = 1
+    while True:
+        yield Crater(id=crater_id,
+                     location=location_func(),
+                     radius=size_distribution.uniform_to_value(np.random.random()),
+                     rim_segments=[(0, 2 * np.pi)])
+        crater_id += 1
--- tests/saturation/test_simulation.py
+++ tests/saturation/test_simulation.py
@@ -1,11 +1,68 @@
 import numpy as np
-from saturation.simulation import get_crater_locations
+
+from saturation.distributions import ProbabilityDistribution
+from saturation.simulation import get_crater_location, get_craters
 
 
-def test_get_crater_locations():
+class DummyProbabilityDistribution(ProbabilityDistribution):
+    def __init__(self):
+        self.uniform_to_value_result = 0
+        self.value_to_probability_result = 0
+        self.probability_to_value_result = 0
+
+    def uniform_to_value(self, uniform: float) -> float:
+        return self.uniform_to_value_result
+
+    def value_to_probability(self, x: float) -> float:
+        return self.value_to_probability_result
+
+    def probability_to_value(self, p: float) -> float:
+        return self.probability_to_value_result
+
+
+def test_get_crater_location():
     # Act
-    result = get_crater_locations(2)
+    result = get_crater_location()
 
     # Assert
-    assert len(result) == 2
-    assert np.all(result == 0)
+    assert np.shape(result) == (2, 2)
+
+
+def test_get_craters_yields_infinitely():
+    # Arrange
+    distribution = DummyProbabilityDistribution()
+
+    # Act
+    result = [next(get_craters(distribution)) for _ in range(5)]
+
+    # Assert
+    assert len(result) == 5
+
+
+def test_get_craters_uses_probability_distribution_and_location_func():
+    # Arrange
+    def location_func():
+        return 7, 13
+    distribution = DummyProbabilityDistribution()
+    distribution.uniform_to_value_result = 37
+
+    # Act
+    result = next(get_craters(distribution, location_func=location_func))
+
+    # Assert
+    assert result.id == 1
+    assert result.location == (7, 13)
+    assert result.radius == 37
+    assert result.rim_segments == [(0, 2 * np.pi)]
+
+
+def test_get_craters_ids_increase():
+    # Arrange
+    distribution = DummyProbabilityDistribution()
+
+    # Act
+    iterator = get_craters(distribution)
+    result = [next(iterator).id for _ in range(5)]
+
+    # Assert
+    assert result == list(range(1, 6))
